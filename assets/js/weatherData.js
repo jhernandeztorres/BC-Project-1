3ac287316e2a7816a6ae5f15a414ce33d757b253
Jose Hernandez Torres <jose@hernandezt.com>
2018-12-05 18:10:07 -0700

    weather js added

---
+++ assets/js/weatherData.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+    let cityArr = [];
+
+    $("#submit").on("click", function () {
+        cityArr = [];
+        let begin = $("#startCity").val().trim();
+        let end = $("#destinationCity").val().trim();
+        if (begin !== "" && end !== ""){
+        cityArr.push({city: begin}, {city: end});
+        weatherData();
+        } else {
+            alert("Fill out all fields");
+        }
+    })
+
+    function weatherData() {
+        const apiKey = "ce836c9b60ad9971ea91f7fa53b647fc";
+        for (let i = 0; i < cityArr.length; i++) {
+            let city = cityArr[i].city;
+            const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + apiKey;
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                var results = response;
+                // console.log(results);
+                let lat = results.coord.lat;
+                let lon = results.coord.lon;
+                let temperature = results.main.temp;
+                let maxTemp = results.main.temp_max;
+                let minTemp = results.main.temp_min;
+                let country = results.sys.country;
+                let weatherMain = results.weather[0].main;
+                let description = results.weather[0].description;
+                let id = "icon" + cityArr.indexOf(cityArr[i]);
+                console.log(id);
+                cityArr[i].lat = lat;
+                cityArr[i].lon = lon;
+                cityArr[i].maxTemp = maxTemp;
+                cityArr[i].minTemp = minTemp;
+                cityArr[i].country = country;
+                cityArr[i].weatherMain = weatherMain;
+                cityArr[i].description = description;
+                cityArr[i].id = id;
+                console.log(cityArr)
+                // Show selected cities name in <div>
+                $("#initial").text(cityArr[0].city);
+                $("#destination").text(cityArr[1].city);
+
+                // Grab temperature from object and show temp in farenheit in <div>
+                const degree = temperature;
+                cityArr[i].temperature = Math.round(degree);
+                $(".initialCity").text("Current Temperature: " + cityArr[0].temperature + "\xB0F");
+                $(".destinationCity").text("Current Temperature: " + cityArr[1].temperature + "\xB0F");
+
+                // Grab lat and lon for Skycons
+                getData(lat, lon, id);
+            })
+        }
+    }
+
+    function getData(lat, lon, id) {
+        const apiKey = "c38f38fafa15b4cc5eb95f4dab2c7dc1";
+        const corsURL = "https://cors-anywhere.herokuapp.com/"; // 
+        let weatherURL = corsURL + "https://api.darksky.net/forecast/" + apiKey + "/" + lat + "," + lon;
+
+        $.getJSON(weatherURL, function (data) {
+            let icon = data.currently.icon;
+            let skycons = new Skycons({
+                "color": "blue"
+            });
+
+            function weatherIcon() {
+                if (icon === "clear-day") {
+                    skycons.add(id, Skycons.CLEAR_DAY);
+                } else if (icon === "clear-night") {
+                    skycons.add(id, Skycons.CLEAR_NIGHT);
+                } else if (icon === "rain") {
+                    skycons.add(id, Skycons.RAIN);
+                } else if (icon === "snow") {
+                    skycons.add(id, Skycons.SNOW);
+                } else if (icon === "sleet") {
+                    skycons.add(id, Skycons.SLEET);
+                } else if (icon === "wind") {
+                    skycons.add(id, Skycons.WIND);
+                } else if (icon === "fog") {
+                    skycons.add(id, Skycons.FOG);
+                } else if (icon === "cloudy") {
+                    skycons.add(id, Skycons.CLOUDY);
+                } else if (icon === "clear-night") {
+                    skycons.add(id, Skycons.CLEAR_NIGHT);
+                } else if (icon === "partly-cloudy-day") {
+                    skycons.add(id, Skycons.PARTLY_CLOUDY_DAY);
+                } else if (icon === "partly-cloudy-night") {
+                    skycons.add(id, Skycons.PARTLY_CLOUDY_NIGHT);
+                } else {
+                    console.log("Dark Sky icon did not return a matching case");
+                }
+                skycons.play();
+            }
+            weatherIcon();
+        })
+    }
+
+});
